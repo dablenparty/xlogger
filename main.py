6d4569b1ca8036200a23217190de47ed53fa24d6
dablenparty <hdavenport8902@gmail.com>
2022-03-21 15:23:26 -0400

    Created demo

---
+++ main.py
@@ -0,0 +1,7 @@
+from inputs import get_gamepad
+
+if __name__ == '__main__':
+    while True:
+        events = get_gamepad()
+        for event in events:
+            print(event.ev_type, event.code, event.state)
