8b9e92e8105e349f4990dec5f17d1703e0b92d6d
dablenparty <hdavenport8902@gmail.com>
2022-04-10 23:30:39 -0400

    Created example project for getting gamepad events

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use gilrs::{Event, EventType, Gilrs};
+
 fn main() {
-    println!("Hello, world!");
+    let mut gilrs = Gilrs::new().unwrap();
+
+    loop {
+        while let Some(Event { event, time: event_time, ..}) = gilrs.next_event() {
+            match event {
+                EventType::AxisChanged(_, _, _) => {}
+                _ => {
+                    println!("{:?} {:?}", event, event_time);
+                }
+            }
+        }
+    }
 }
