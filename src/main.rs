d4aa826d42b4c6a0c1c5cf3590816f7df1455778
dablenparty <hdavenport8902@gmail.com>
2022-04-13 20:30:39 -0400

    Switched to if let for ButtonChanged event

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,9 @@ fn main() {
     let mut gilrs = Gilrs::new().unwrap();
 
     loop {
-        while let Some(Event { event, time: event_time, ..}) = gilrs.next_event() {
-            match event {
-                EventType::AxisChanged(_, _, _) => {}
-                _ => {
-                    println!("{:?} {:?}", event, event_time);
-                }
+        while let Some(Event { event, time: event_time, .. }) = gilrs.next_event() {
+            if let EventType::ButtonChanged(_, _, _) = event {
+                println!("{:?} {:?}", event, event_time);
             }
         }
     }
