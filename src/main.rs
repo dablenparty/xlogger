f5a19bf4d5f83b9688be5a4924bf3abaca344a9c
dablenparty <hdavenport8902@gmail.com>
2022-04-19 23:21:00 -0400

    Runs visualize script on exit

--- src/main.rs
+++ src/main.rs
@@ -46,9 +46,12 @@ fn main() {
         File::create(&csv_path).unwrap(),
     )));
 
+    // clone the writer into the closure
     let writer_clone = csv_lock.clone();
+    let csv_path_clone = csv_path.clone();
     ctrlc::set_handler(move || {
         println!("received ctrl-c");
+        // this ensures that the writer has been properly flushed before the program exits
         writer_clone
             .lock()
             .unwrap_or_else(|_| {
@@ -57,6 +60,33 @@ fn main() {
             })
             .flush()
             .unwrap();
+        // pass the data file to the visualize script
+        let data_file = &csv_path_clone;
+        let visualize_script = std::env::current_exe()
+            .unwrap_or_else(|_| PathBuf::from("."))
+            .parent()
+            .unwrap_or_else(|| Path::new("."))
+            .join("visualize")
+            .join("visualize");
+        // spawn the visualize script and wait for it to finish
+        let mut proc_handle = std::process::Command::new(visualize_script)
+            .arg(data_file)
+            .spawn()
+            .unwrap_or_else(|_| {
+                eprintln!("failed to spawn visualize script");
+                std::process::exit(1);
+            });
+        // wait for the process to finish
+        let exit_status = proc_handle.wait().unwrap_or_else(|_| {
+            eprintln!("failed to wait for visualize script");
+            std::process::exit(1);
+        });
+
+        if !exit_status.success() {
+            eprintln!("visualize script exited with non-zero status");
+            std::process::exit(1);
+        }
+
         std::process::exit(0);
     })
     .expect("Error setting the Ctrl-C handler");
@@ -80,6 +110,10 @@ fn main() {
                     let duration = event_time.duration_since(down_time).unwrap();
                     time_map.insert(name.clone(), SystemTime::UNIX_EPOCH);
                     println!("{} released after {:?}", name, duration);
+                    // this should be ok since the lock will always be acquired by this thread
+                    // the only time it could be acquired by another thread is if the program
+                    // is exiting, in which case the lock will be dropped and the writer will
+                    // be flushed
                     let mut csv_writer = csv_lock.lock().unwrap_or_else(|_| {
                         eprintln!("failed to lock csv_writer");
                         std::process::exit(1);
