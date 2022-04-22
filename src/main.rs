f1e69e54630480637361e9c21d84a28fd8853e5e
dablenparty <hdavenport8902@gmail.com>
2022-04-21 23:36:19 -0400

    Renamed App struct

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,17 @@ use std::thread;
 use eframe::{egui, epi};
 use gilrs::{EventType, Gamepad, GamepadId, Gilrs};
 
-struct MyEguiApp {
+struct XloggerApp {
     should_run: Arc<AtomicBool>,
 }
 
-impl MyEguiApp {
+impl XloggerApp {
     fn new(should_run: Arc<AtomicBool>) -> Self {
         Self { should_run }
     }
 }
 
-impl epi::App for MyEguiApp {
+impl epi::App for XloggerApp {
     fn update(&mut self, ctx: &egui::Context, frame: &epi::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             let should_run_value = self.should_run.load(std::sync::atomic::Ordering::Relaxed);
@@ -51,7 +51,7 @@ fn main() {
         });
     };
 
-    let app = MyEguiApp::new(should_run);
+    let app = XloggerApp::new(should_run);
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(Box::new(app), native_options);
 }
