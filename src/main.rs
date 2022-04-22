6d30e263b5fd66f80cf14f9f676fc335fc43206d
dablenparty <hdavenport8902@gmail.com>
2022-04-21 22:38:55 -0400

    Created simple egui demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use eframe::{egui, epi};
+
+#[derive(Default)]
+struct MyEguiApp {
+    is_running: bool,
+}
+
+impl epi::App for MyEguiApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &epi::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let text = if self.is_running { "Stop" } else { "Start" };
+            if ui.button(text).clicked() {
+                self.is_running = !self.is_running;
+            }
+        });
+    }
+
+    fn name(&self) -> &str {
+        "xlogger"
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let app = MyEguiApp::default();
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(Box::new(app), native_options);
 }
