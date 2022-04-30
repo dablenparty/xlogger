e4ee7bc5a9c6973641c36d6432cd47f41d79f23b
dablenparty <hdavenport8902@gmail.com>
2022-04-30 19:03:30 -0400

    Removed unused function

--- src/lib.rs
+++ src/lib.rs
@@ -34,28 +34,6 @@ struct ControllerStickState {
     y: f32,
 }
 
-/// Runs the main event loop for xlogger. This function is designed to be run on a separate thread.
-///
-/// This entails listening for events from the gamepads and logging them to the CSV data file.
-///
-/// # Arguments
-///
-/// * `should_run`: A thread-safe boolean that is set to false when the event loop should stop.
-///
-/// returns: ()
-pub fn run_event_loop(should_run: Arc<AtomicBool>) {
-    let mut gilrs = Gilrs::new().unwrap();
-
-    loop {
-        while let Some(gilrs::Event { event, id, .. }) = gilrs.next_event() {
-            if !should_run.load(std::sync::atomic::Ordering::Relaxed) {
-                continue;
-            }
-            println!("{:?} {:?}", id, event);
-        }
-    }
-}
-
 pub fn listen_for_events(should_run: Arc<AtomicBool>) {
     // TODO: return a Result so the errors can be handled externally
     let mut gilrs = Gilrs::new().expect("failed to initialize controller processor");
