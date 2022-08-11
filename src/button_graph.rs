961fd476cb99d98e3a51548498694beb42737f2f
dablenparty <hdavenport8902@gmail.com>
2022-08-11 13:03:37 -0400

    Added date option to button graph

--- src/button_graph.rs
+++ src/button_graph.rs
@@ -1,5 +1,6 @@
 use std::{collections::HashMap, ffi::OsStr, ops::RangeInclusive, path::PathBuf};
 
+use chrono::{DateTime, NaiveDateTime, Utc};
 use eframe::egui::{
     plot::{BoxElem, BoxPlot, BoxSpread, Legend, Plot},
     Context, Ui, Window,
@@ -9,11 +10,13 @@ use log::info;
 use crate::{BoxedResult, ControllerButtonEvent, EguiView};
 
 const DATETIME_FORMAT: &str = "%b %e, %Y %H:%M:%S";
+const TIME_FORMAT: &str = "%H:%M:%S";
 
 pub struct ControllerButtonGraph {
     csv_data: Option<HashMap<String, Vec<BoxElem>>>,
     data_path: Option<PathBuf>,
     plot_id: String,
+    show_date: bool,
 }
 
 impl Default for ControllerButtonGraph {
@@ -21,6 +24,7 @@ impl Default for ControllerButtonGraph {
         Self {
             csv_data: None,
             data_path: None,
+            show_date: false,
             plot_id: uuid::Uuid::new_v4().to_string(),
         }
     }
@@ -38,6 +42,7 @@ impl ControllerButtonGraph {
     /// This function will return an error if the CSV data is invalid or not found.
     pub fn load(&mut self, data_path: PathBuf) -> BoxedResult<()> {
         info!("loading button data from {}", data_path.display());
+        // TODO: move element construction to ui function
         let data = csv::Reader::from_path(&data_path)?
             .deserialize::<ControllerButtonEvent>()
             .try_fold::<_, _, BoxedResult<HashMap<String, Vec<BoxElem>>>>(
@@ -45,9 +50,9 @@ impl ControllerButtonGraph {
                 |mut acc, result| {
                     let event = result?;
                     let duration = event.release_time - event.press_time;
-                    let as_datetime = chrono::DateTime::<chrono::Utc>::from_utc(
-                        chrono::NaiveDateTime::from_timestamp(event.press_time as i64, 0),
-                        chrono::Utc,
+                    let as_datetime = DateTime::<Utc>::from_utc(
+                        NaiveDateTime::from_timestamp(event.press_time as i64, 0),
+                        Utc,
                     );
                     let elem_name = format!(
                         "Button: {}\nPressed at: {}\nHeld for: {:.2}s",
@@ -126,15 +131,19 @@ impl EguiView for ControllerButtonGraph {
             })
             .collect();
 
+        let format = if self.show_date {
+            DATETIME_FORMAT
+        } else {
+            TIME_FORMAT
+        };
         let x_fmt = |x: f64, _range: &RangeInclusive<f64>| {
             // format to datetime string
-            let datetime = chrono::DateTime::<chrono::Utc>::from_utc(
-                chrono::NaiveDateTime::from_timestamp(x as i64, 0),
-                chrono::Utc,
-            );
-            datetime.format(DATETIME_FORMAT).to_string()
+            let datetime =
+                DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(x as i64, 0), Utc);
+            datetime.format(format).to_string()
         };
 
+        ui.checkbox(&mut self.show_date, "Show date");
         Plot::new(self.plot_id.clone())
             .legend(Legend::default())
             .x_axis_formatter(x_fmt)
