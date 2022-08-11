61adbfaae62f3b5eb79010c77aedf17af19ae3e0
dablenparty <hdavenport8902@gmail.com>
2022-08-10 20:42:10 -0400

    Extracted button graphs to windows

---
+++ src/button_graph.rs
@@ -0,0 +1,100 @@
+use std::{collections::HashMap, ffi::OsStr, path::PathBuf};
+
+use eframe::egui::{
+    plot::{BoxElem, BoxPlot, BoxSpread, Legend, Plot},
+    Context, Ui, Window,
+};
+use log::info;
+
+use crate::{BoxedResult, ControllerButtonEvent};
+
+#[derive(Default)]
+pub struct ControllerButtonGraph {
+    csv_data: Option<HashMap<String, Vec<BoxElem>>>,
+    data_path: Option<PathBuf>,
+}
+
+impl ControllerButtonGraph {
+    pub fn load(&mut self, data_path: PathBuf) -> BoxedResult<()> {
+        info!("loading button data from {}", data_path.display());
+        let data = csv::Reader::from_path(&data_path)?
+            .deserialize::<ControllerButtonEvent>()
+            .try_fold::<_, _, BoxedResult<HashMap<String, Vec<BoxElem>>>>(
+                HashMap::new(),
+                |mut acc, result| {
+                    let event = result?;
+                    let box_elem = BoxElem::new(
+                        0.5,
+                        BoxSpread::new(
+                            event.press_time,
+                            event.press_time,
+                            event.press_time,
+                            event.release_time,
+                            event.release_time,
+                        ),
+                    );
+                    match acc.get_mut(&event.button) {
+                        Some(vec) => vec.push(box_elem),
+                        None => {
+                            acc.insert(event.button, vec![box_elem]);
+                        }
+                    }
+                    Ok(acc)
+                },
+            )?;
+        self.data_path = Some(data_path);
+        self.csv_data = Some(data);
+        Ok(())
+    }
+
+    pub fn show(&mut self, ctx: &Context, open: &mut bool) {
+        let title = if let Some(path) = self.data_path.as_ref() {
+            path.as_path()
+                .file_name()
+                .unwrap_or(OsStr::new("Button Graph"))
+                .to_string_lossy()
+                .into_owned()
+        } else {
+            "No data loaded".to_string()
+        };
+        Window::new(title)
+            .resizable(true)
+            .collapsible(true)
+            .title_bar(true)
+            .open(open)
+            .show(ctx, |ui| self.ui(ui));
+    }
+
+    fn ui(&mut self, ui: &mut Ui) {
+        if self.csv_data.is_none() {
+            ui.label("No data loaded");
+            return;
+        }
+        let data = self.csv_data.as_ref().unwrap();
+        let box_plots: Vec<BoxPlot> = data
+            .iter()
+            .enumerate()
+            .map(|(i, (key, vec))| {
+                let mapped_boxes: Vec<BoxElem> = vec
+                    .to_vec()
+                    .into_iter()
+                    .map(|mut e| {
+                        e.argument = i as f64;
+                        e
+                    })
+                    .collect();
+                BoxPlot::new(mapped_boxes).name(key).horizontal()
+            })
+            .collect();
+
+        // TODO: generate a unique ID
+        Plot::new("Button Data")
+            .legend(Legend::default())
+            .show(ui, |plot_ui| {
+                box_plots
+                    .into_iter()
+                    .for_each(|box_plot| plot_ui.box_plot(box_plot));
+            })
+            .response;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use human_panic::setup_panic;
 use image::ImageResult;
 use log::{error, info, LevelFilter};
 use simplelog::{Config, WriteLogger};
+use xlogger::button_graph::ControllerButtonGraph;
 use xlogger::stick_graph::ControllerStickGraph;
 use xlogger::{open_dialog_in_data_folder, BoxedResult, ControllerButtonEvent, StatefulText};
 
@@ -22,18 +23,11 @@ use crate::util::{create_dir_if_not_exists, get_exe_parent_dir};
 mod util;
 
 #[derive(Default)]
-struct ButtonGraphProps {
-    csv_data: Option<HashMap<String, Vec<BoxElem>>>,
-    data_path: Option<PathBuf>,
-    show_graph: bool,
-}
-
-#[derive(Default)]
 struct XloggerApp {
     should_run: Arc<AtomicBool>,
     saved_text: StatefulText,
     stick_graphs: Vec<(bool, ControllerStickGraph)>,
-    button_graph_props: ButtonGraphProps,
+    button_graphs: Vec<(bool, ControllerButtonGraph)>,
 }
 
 impl eframe::App for XloggerApp {
@@ -81,9 +75,12 @@ impl eframe::App for XloggerApp {
                 };
                 if ui.button("Visualize Buttons").clicked() {
                     if let Some(path) = open_dialog_in_data_folder() {
-                        self.button_graph_props.data_path = Some(path);
-                        self.button_graph_props.show_graph = true;
-                        self.button_graph_props.csv_data = None
+                        let mut graph = ControllerButtonGraph::default();
+                        if let Err(e) = graph.load(path) {
+                            error!("{:?}", e);
+                        } else {
+                            self.button_graphs.push((true, graph));
+                        }
                     }
                 }
             });
@@ -94,101 +91,13 @@ impl eframe::App for XloggerApp {
                 });
             // remove the stick graphs that are closed (they're set to show when they're created)
             self.stick_graphs.retain(|(show_graph, _)| *show_graph);
-            // TODO: extract windows into their own impl structs
-            if self.button_graph_props.show_graph {
-                let window = egui::Window::new("Button Graph")
-                    .resizable(true)
-                    .collapsible(true)
-                    .title_bar(true);
-                window.show(ctx, |ui| {
-                    if let Err(e) = self.visualize_button_data(ui) {
-                        error!(
-                            "Something went wrong deserializing data at {:#?}:",
-                            self.button_graph_props.data_path,
-                        );
-                        error!("{}", e);
-                        self.button_graph_props.show_graph = false;
-                        self.button_graph_props.csv_data = None;
-                        self.button_graph_props.data_path = None;
-                    }
+            self.button_graphs
+                .iter_mut()
+                .for_each(|(show_graph, graph)| {
+                    graph.show(ctx, show_graph);
                 });
-            }
+            self.button_graphs.retain(|(show_graph, _)| *show_graph);
         });
-    }
-}
-
-impl XloggerApp {
-    /// Visualizes the data in the given file.
-    ///
-    /// # Arguments
-    ///
-    /// * `path` - The path to the file to visualize.
-    ///
-    /// returns: `io::Result<ExitStatus>`
-    fn visualize_button_data(&mut self, ui: &mut Ui) -> BoxedResult<Option<egui::Response>> {
-        let button_graph_props = &mut self.button_graph_props;
-        if !button_graph_props.show_graph {
-            return Ok(None);
-        }
-        let data_path = button_graph_props.data_path.as_ref().unwrap();
-        let data = if let Some(data) = button_graph_props.csv_data.as_ref() {
-            data.clone()
-        } else {
-            // this looks horrible because the data needs to be sorted by button, not timestamp
-            info!("loading button data from {}", data_path.display());
-            let data = csv::Reader::from_path(data_path)?
-                .deserialize::<ControllerButtonEvent>()
-                .try_fold::<_, _, BoxedResult<HashMap<String, Vec<BoxElem>>>>(
-                    HashMap::new(),
-                    |mut acc, result| {
-                        let event = result?;
-                        let box_elem = BoxElem::new(
-                            0.5,
-                            BoxSpread::new(
-                                event.press_time,
-                                event.press_time,
-                                event.press_time,
-                                event.release_time,
-                                event.release_time,
-                            ),
-                        );
-                        match acc.get_mut(&event.button) {
-                            Some(vec) => vec.push(box_elem),
-                            None => {
-                                acc.insert(event.button, vec![box_elem]);
-                            }
-                        }
-                        Ok(acc)
-                    },
-                )?;
-            self.button_graph_props.csv_data = Some(data.clone());
-            data
-        };
-        let box_plots: Vec<BoxPlot> = data
-            .iter()
-            .enumerate()
-            .map(|(i, (key, vec))| {
-                let mapped_boxes: Vec<BoxElem> = vec
-                    .to_vec()
-                    .into_iter()
-                    .map(|mut e| {
-                        e.argument = i as f64;
-                        e
-                    })
-                    .collect();
-                BoxPlot::new(mapped_boxes).name(key).horizontal()
-            })
-            .collect();
-        Ok(Some(
-            Plot::new("Button Presses")
-                .legend(Legend::default())
-                .show(ui, |plot_ui| {
-                    box_plots
-                        .into_iter()
-                        .for_each(|box_plot| plot_ui.box_plot(box_plot));
-                })
-                .response,
-        ))
     }
 }
 
