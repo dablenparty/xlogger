beea958ba4846572d4804fac36e37b933112ee25
dablenparty <hdavenport8902@gmail.com>
2022-08-27 20:00:12 -0400

    Updated build script to include Info.plist, icon

--- release.rs
+++ release.rs
@@ -3,18 +3,40 @@ use std::{fs, io, process::Command};
 
 #[cfg(target_os = "macos")]
 fn main() -> Result<(), io::Error> {
-    // TODO: package icon
-    println!("building xlogger.app");
-    const XLOGGER_TARGET: &str = "target/release/macos/xlogger";
+    use std::path::PathBuf;
+
+    const XLOGGER_TARGET: &str = "target/release/macos/xlogger/xlogger.app";
+    const CARGO_PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
+    if PathBuf::from(XLOGGER_TARGET).exists() {
+        println!("removing old xlogger.app");
+        fs::remove_dir_all(XLOGGER_TARGET)?;
+    }
+    // compile in release
     Command::new("cargo")
         .args(["build", "--release", "--bin", "xlogger"])
         .status()?;
-    fs::create_dir_all(format!("{}/xlogger.app/Contents/MacOS", XLOGGER_TARGET))?;
+    println!("building xlogger.app");
+    // copy binary
+    let contents_folder = format!("{}/Contents", XLOGGER_TARGET);
+    fs::create_dir_all(format!("{}/MacOS", contents_folder))?;
     fs::copy(
         "target/release/xlogger",
-        format!("{}/xlogger.app/Contents/MacOS/xlogger", XLOGGER_TARGET),
+        format!("{}/MacOS/xlogger", contents_folder),
     )?;
+    // copy icon
+    let resources_folder = format!("{}/Resources", contents_folder);
+    fs::create_dir_all(&resources_folder)?;
+    let icon_path = format!("{}/icon.icns", resources_folder);
+    fs::copy("assets/icon.icns", icon_path)?;
+    // format and copy Info.plist
+    //* MacOS parses whitespace in Info.plist as significant, so don't format with extra newlines and spaces
+    let plist_path = format!("{}/Info.plist", contents_folder);
+    let plist_text = fs::read_to_string("assets/macos/Info.plist")?
+        .replace("{XLOGGER_BUNDLE_VERSION}", CARGO_PKG_VERSION)
+        .replace("{XLOGGER_BUNDLE_VERSION_SHORT}", CARGO_PKG_VERSION);
+    fs::write(plist_path, plist_text)?;
     println!("packaging xlogger.app");
+    // package into dmg
     Command::new("hdiutil")
         .args([
             "create",
