3157ef60c48402c9511dda0ea5f2eb653846ad69
dablenparty <hdavenport8902@gmail.com>
2022-10-12 23:08:06 -0400

    Stick graph now shows timestamp range for data

--- src/stick_graph.rs
+++ src/stick_graph.rs
@@ -6,12 +6,13 @@ use eframe::egui::{
 };
 use log::{info, warn};
 
-use crate::{ControllerStickEvent, CsvLoad, EguiView};
+use crate::{util::f64_to_formatted_time, ControllerStickEvent, CsvLoad, EguiView};
 
 #[derive(Clone)]
 struct ControllerStickData {
     left_values: Vec<[f64; 2]>,
     right_values: Vec<[f64; 2]>,
+    timestamps: Vec<f64>,
 }
 
 pub struct ControllerStickGraph {
@@ -39,21 +40,27 @@ impl Default for ControllerStickGraph {
 impl CsvLoad for ControllerStickGraph {
     fn load(&mut self, data_path: PathBuf) -> csv::Result<()> {
         info!("Loading stick data from {}", data_path.display());
-        let (ls_events, rs_events) = csv::Reader::from_path(&data_path)?
+        let (left_values, right_values, timestamps) = csv::Reader::from_path(&data_path)?
             .deserialize::<ControllerStickEvent>()
             .try_fold::<_, _, Result<_, csv::Error>>(
-                (Vec::<[f64; 2]>::new(), Vec::<[f64; 2]>::new()),
+                (
+                    Vec::<[f64; 2]>::new(),
+                    Vec::<[f64; 2]>::new(),
+                    Vec::<f64>::new(),
+                ),
                 |mut acc, result| {
                     let event = result?;
                     acc.0.push([event.left_x, event.left_y]);
                     acc.1.push([event.right_x, event.right_y]);
+                    acc.2.push(event.time);
                     Ok(acc)
                 },
             )?;
         self.data_path = Some(data_path);
         self.csv_data = Some(ControllerStickData {
-            left_values: ls_events,
-            right_values: rs_events,
+            left_values,
+            right_values,
+            timestamps,
         });
         Ok(())
     }
@@ -86,24 +93,29 @@ impl EguiView for ControllerStickGraph {
         let data = self.csv_data.as_ref().unwrap();
         // use a bit shift since egui is immediate mode
         let midpoint = self.data_offset >> 1; // divide by 2
-        let ls_sliced = &data.left_values[self.slider_timestamp.saturating_sub(midpoint.into())
-            ..self
-                .slider_timestamp
-                .saturating_add(midpoint.into())
-                .min(data.left_values.len())];
-        let rs_sliced = &data.right_values[self.slider_timestamp.saturating_sub(midpoint.into())
-            ..self
-                .slider_timestamp
-                .saturating_add(midpoint.into())
-                .min(data.right_values.len())];
+        let left_offset_timestamp = self.slider_timestamp.saturating_sub(midpoint.into());
+        let right_offset_timestamp = self.slider_timestamp.saturating_add(midpoint.into());
+
+        let ls_sliced = &data.left_values
+            [left_offset_timestamp..right_offset_timestamp.min(data.left_values.len())];
+        let rs_sliced = &data.right_values
+            [left_offset_timestamp..right_offset_timestamp.min(data.right_values.len())];
+
         let ls_values = PlotPoints::new(ls_sliced.to_vec());
         // shift the right stick values to the right so they don't overlap the left stick
         let rs_values = PlotPoints::new(rs_sliced.iter().map(|v| [v[0] + 2.5, v[1]]).collect());
+
         ui.horizontal(|ui| {
             ui.label("Time");
             let left_len = data.left_values.len();
             ui.add(Slider::new(&mut self.slider_timestamp, 0..=left_len));
-            ui.checkbox(&mut self.show_lines, "Show lines");
+            let base_time = data.timestamps.first().unwrap_or(&0.0);
+            let start_time_string =
+                f64_to_formatted_time(data.timestamps[left_offset_timestamp] - base_time);
+            let end_time_string = f64_to_formatted_time(
+                data.timestamps[right_offset_timestamp.min(left_len - 1)] - base_time,
+            );
+            ui.label(format!("{} - {}", start_time_string, end_time_string));
             if ls_sliced.len() == usize::MAX {
                 let text = "Warning: too much data to visualize! not all of it will be shown";
                 warn!("{}", text);
@@ -114,6 +126,7 @@ impl EguiView for ControllerStickGraph {
             ui.label("Number of points");
             ui.add(Slider::new(&mut self.data_offset, u8::MIN..=u8::MAX))
                 .on_hover_text("Higher values may affect performance");
+            ui.checkbox(&mut self.show_lines, "Show lines");
         });
         Plot::new(self.plot_id)
             .data_aspect(1.0)
